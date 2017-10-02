5b95a980ee2d07a33f25d46e0c5bf73b744894fa
finnhodgkin <finnhodgkin@gmail.com>
2017-10-02 12:50:34 +0100

    fixes form success message and restyles the contact message #396

--- js/app_google_form_handler.js
+++ js/app_google_form_handler.js
@@ -1,19 +1,5 @@
-function validEmail(email) { // see:
-  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-  return re.test(email);
-}
-
-function validateHuman(honeypot) {
-  if (honeypot) {  //if hidden form filled up
-    console.log("Robot Detected!");
-    return true;
-  } else {
-    console.log("Welcome Human!");
-  }
-}
-
 // get all data in form and return object
-function getFormData() {
+function getBetaFormData() {
   var elements = document.getElementById("appform").elements; // all form elements
   var fields = Object.keys(elements).map(function(k) {
     if(elements[k].name !== undefined) {
@@ -51,9 +37,9 @@ function getFormData() {
   return data;
 }
 
-function handleFormSubmit(event) {  // handles form submit withtout any jquery
+function handleBetaFormSubmit(event) {  // handles form submit withtout any jquery
   event.preventDefault();           // we are submitting via xhr below
-  var data = getFormData();         // get the values submitted in the form
+  var data = getBetaFormData();         // get the values submitted in the form
 
   /* OPTION: Remove this comment to enable SPAM prevention, see README.md
   if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
@@ -84,10 +70,10 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     xhr.send(encoded);
   }
 }
-function loaded() {
+function loadedBeta() {
   console.log('contact form submission handler loaded successfully');
   // bind to the submit event of our form
   var form = document.getElementById('appform');
-  form.addEventListener("submit", handleFormSubmit, false);
+  form.addEventListener("submit", handleBetaFormSubmit, false);
 };
-document.addEventListener('DOMContentLoaded', loaded, false);
+document.addEventListener('DOMContentLoaded', loadedBeta, false);
